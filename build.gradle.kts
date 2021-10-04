ac120bc7f0196f635c0589dcdc047fca805078ef
Andrei Egorov <aegorov4@alfabank.ru>
2021-10-04 15:25:00 +0300

    testing2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,27 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-
-    id("application")
+//    id("application")
     id("org.springframework.boot") version "2.5.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.spring") version "1.5.31"
 }
 
-apply{
-    plugin("kotlin")
-    plugin("kotlin-spring")
-    plugin("java")
-    plugin("org.springframework.boot")
-}
+//apply{
+//    plugin("kotlin")
+//    plugin("kotlin-spring")
+//    plugin("java")
+//    plugin("org.springframework.boot")
+//}
 
 group = "heroku.test"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
-application {
-    mainClass.set("heroku.test.first.FirstApplication")
-}
+//application {
+//    mainClass.set("heroku.test.first.FirstApplication")
+//}
 
 
 repositories {
@@ -43,6 +42,22 @@ tasks.withType<KotlinCompile> {
         jvmTarget = "11"
     }
 }
+
+//tasks.withType<Jar> {
+//    duplicatesStrategy = DuplicatesStrategy.WARN
+//    // Otherwise you'll get a "No main manifest attribute" error
+//    manifest {
+//        attributes["Main-Class"] = "heroku.test.first.FirstApplicationKt"
+//    }
+//
+//    // To add all of the dependencies otherwise a "NoClassDefFoundError" error
+//    from(sourceSets.main.get().output)
+//
+//    dependsOn(configurations.runtimeClasspath)
+//    from({
+//        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+//    })
+//}
 
 tasks.withType<Test> {
     useJUnitPlatform()
